96cc2cdc0ac2bc6bea7e4904208ca0d078228687
Antonious-Awad <antonuostony1@gmail.com>
2021-08-08 16:28:43 +0200

    added fonts and images

--- lib/modules/on_boarding_screen.dart
+++ lib/modules/on_boarding_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 class OnBoardingScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage("assets/images/onboard_1.jpg"),
+          ),
+          SizedBox(height: 30,),
+          Text(
+            "Screen Title",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
