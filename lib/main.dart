e758007887010a883ec06b80988bbc1b67abb6fd
Antonious-Awad <antonuostony1@gmail.com>
2021-08-08 15:56:17 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/layout/cubit/shop_cubit.dart';
+import 'package:shop_app/modules/on_boarding_screen.dart';
+import 'package:shop_app/shared/bloc_observer.dart';
+import 'package:shop_app/shared/styles/themes.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ShopCubit(),
+      child: BlocConsumer<ShopCubit, ShopState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: OnBoardingScreen(),
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/shared/styles/themes.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+ThemeData lightTheme = ThemeData(
+  primarySwatch: Colors.deepOrange,
+  floatingActionButtonTheme:
+      FloatingActionButtonThemeData(backgroundColor: Colors.deepOrange),
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      backwardsCompatibility: false,
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.dark),
+      color: Colors.white,
+      elevation: 0,
+      titleTextStyle: TextStyle(
+          color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),
+      centerTitle: true),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: Colors.deepOrange,
+    elevation: 30,
+  ),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+        fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black),
+  ),
+);
+
+ThemeData darkTheme = ThemeData(
+  textTheme: TextTheme(
+      bodyText1: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white)),
+  scaffoldBackgroundColor: HexColor('333739'),
+  primarySwatch: Colors.deepOrange,
+  floatingActionButtonTheme:
+      FloatingActionButtonThemeData(backgroundColor: Colors.deepOrange),
+  appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+      backwardsCompatibility: false,
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: HexColor('333739'),
+          statusBarIconBrightness: Brightness.light),
+      color: HexColor('333739'),
+      elevation: 0,
+      titleTextStyle: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+      centerTitle: true),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: HexColor('333739'),
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.deepOrange,
+      elevation: 30,
+      unselectedItemColor: Colors.grey),
+);
